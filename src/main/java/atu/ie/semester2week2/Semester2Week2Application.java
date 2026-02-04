6b7f7de049744b8dd2e77609a405de9ba9581692
Beatrice Papi <G00461762@atu.ie>
2026-02-04 00:20:51 +0000

    Created Project

---
+++ src/main/java/atu/ie/semester2week2/Semester2Week2Application.java
@@ -0,0 +1,13 @@
+package atu.ie.semester2week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semester2Week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semester2Week2Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/semester2week2/Semester2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.semester2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semester2Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
