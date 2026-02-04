b842b2e1b9da48cf822d38865c0095d36f3c83e0
Beatrice Papi <G00461762@atu.ie>
2026-02-04 01:07:34 +0000

    Added two endpoints for hello and greet

---
+++ src/main/java/atu/ie/semester2week2/RequestController.java
@@ -0,0 +1,21 @@
+package atu.ie.semester2week2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RequestController {
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name)
+    {
+        return "Hello "+name;
+    }
+}
